4585057f98fae1b988ee93554423f9be162da852
dim4ss <dimasghifaria>
2024-11-28 16:48:21 +0700

    desain-fix

--- resources/views/Page/Order/tambah_order.blade.php
+++ resources/views/Page/Order/tambah_order.blade.php
@@ -70,7 +70,7 @@
             </div>
             <div class="flex items-end">
                 <button type="submit"
-                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add</button>
             </div>
         </form>
         <div class="flex mt-8">
@@ -107,6 +107,10 @@
                 {{-- <div class="mt-4">
                     {{ $data->onEachSide(1)->links('vendor.pagination') }}
                 </div> --}}
+                <div class="flex items-end mt-5">
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Selesai</button>
+                </div>
             </div>
         </div>
     </div>
